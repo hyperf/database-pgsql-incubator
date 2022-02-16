74bc1ec3f01e1e8b6737997a537577125797cc4f
庄远辉 <zhuangyuanhui@kkguan.com>
2022-02-16 11:57:46 +0800

    add ConfigProvider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Database\PgSQL;
+
+use Hyperf\Database\PgSQL\Listener\RegisterConnectionListener;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'listeners' => [
+                RegisterConnectionListener::class
+            ],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
