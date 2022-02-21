274fe3caa850550c02b748003523c4a599a6f0bb
庄远辉 <zhuangyuanhui@kkguan.com>
2022-02-21 15:22:47 +0800

    modify .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -84,6 +84,6 @@ return (new PhpCsFixer\Config())
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->exclude('vendor')
-            ->in(__DIR__)
+            ->in([__DIR__.'/src/', __DIR__.'/tests/'])
     )
     ->setUsingCache(false);
